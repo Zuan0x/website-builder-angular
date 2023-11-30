0f06d8f3eab1cca25f5bf985273fb5670d050b02
Zuan0x <zuan.0x@gmail.com>
2023-11-30 15:10:00 +0000

    Add sidebar component

--- src/app/layout/layout.component.ts
+++ src/app/layout/layout.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MatGridListModule } from '@angular/material/grid-list';
+import { SidebarComponent } from '../sidebar/sidebar.component';
 
 @Component({
   selector: 'app-layout',
   standalone: true,
-  imports: [CommonModule, MatGridListModule],
+  imports: [CommonModule, MatGridListModule, SidebarComponent],
   templateUrl: 'layout.component.html',
   styles: [
   ]
